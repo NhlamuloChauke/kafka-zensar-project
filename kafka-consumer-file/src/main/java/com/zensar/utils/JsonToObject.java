2f1c23610f54bd89a874154b9894e821f0268b9f
nhlamulo <hlanulani@gmail.com>
2021-07-18 13:55:33 +0200

    modified JsonToObject class

--- kafka-consumer-file/src/main/java/com/zensar/utils/JsonToObject.java
+++ kafka-consumer-file/src/main/java/com/zensar/utils/JsonToObject.java
@@ -1,18 +1,21 @@
 package com.zensar.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * 
  * @author NChauke
- * @since  18/07/2021
+ * @since 18/07/2021
  *
  */
 public class JsonToObject {
+	private static final Logger LOG = LoggerFactory.getLogger(JsonToObject.class);
 
-	public static <T> T processing(Class<T> type, String message) throws JsonMappingException, JsonProcessingException {
+	public static <T> T processing(Class<T> type, String message) {
 
 		// ObjectMapper instantiation
 		ObjectMapper mapper = new ObjectMapper();
@@ -20,9 +23,13 @@ public class JsonToObject {
 		message = message.replace("\\n", "");
 		message = message.replaceAll("^\"|\"$|\\\\", "");
 
-		// Deserialization into the `T type` class
-		T entity = mapper.readValue(message, type);
-
+		T entity = null;
+		try {
+			// Deserialization into the `T type` class
+			entity = mapper.readValue(message, type);
+		} catch (JsonProcessingException e) {
+			LOG.error("Error Message Json Mapping:{}", e.getMessage());
+		}
 		return entity;
 	}
 }
