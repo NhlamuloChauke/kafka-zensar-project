2d63872ba802f8b6344f2c510f2e8668852a226e
nhlamulo <hlanulani@gmail.com>
2021-07-18 13:34:41 +0200

    modified dbinit

--- mysql-init/dbinit.sql
+++ mysql-init/dbinit.sql
@@ -8,3 +8,12 @@ GRANT ALL PRIVILEGES ON KafkaData.* TO 'root'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON KafkaData.* TO 'root'@'localhost' IDENTIFIED BY 'password';
 
 use KafkaData;
+
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE `person` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
